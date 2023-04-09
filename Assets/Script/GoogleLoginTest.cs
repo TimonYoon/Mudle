ac9362f4699eda71f39d250400ce679c7895e5c9
timonyoon <dlskatm@gmail.com>
2023-04-10 00:31:44 +0900

    정상적 빌드 가능하도록 플러그인 정리

--- Assets/Script/GoogleLoginTest.cs
+++ Assets/Script/GoogleLoginTest.cs
@@ -1,12 +1,8 @@
 ﻿using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 using UnityEngine.SocialPlatforms;
-
-
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
-
 using UnityEngine.UI;
 
 public class GoogleLoginTest : MonoBehaviour
@@ -16,30 +12,30 @@ public class GoogleLoginTest : MonoBehaviour
     {
         Instance = this;
     }
+
+    
     //public Text text_Test;
     void Start()
     {
-#if UNITY_ANDROID
-
-        // Create client configuration
-        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
-
-        // Enable debugging output (recommended)
-        PlayGamesPlatform.DebugLogEnabled = true;
-
-        // Initialize and activate the platform
-        PlayGamesPlatform.InitializeInstance(config);
-        PlayGamesPlatform.Activate();
-        SignIn();
-
-
-#elif UNITY_IOS
- 
-        GameCenterPlatform.ShowDefaultAchievementCompletionBanner(true);
- 
-#endif
-
-
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+    private void ProcessAuthentication(SignInStatus status) 
+    {
+        Debug.Log($"ProcessAuthentication {status}");
+        if (status == SignInStatus.Success)
+        {
+            //디버깅에 권장됨
+            PlayGamesPlatform.DebugLogEnabled = true;
+            //PlayGamesPlatform 활성화
+            PlayGamesPlatform.Activate();
+            SignIn();
+        } 
+        else 
+        {
+            // Disable your integration with Play Games Services or show a login button
+            // to ask users to sign-in. Clicking it should call
+            // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+        }
     }
 
     public void SignIn()
--- Assets/Script/ScreenShot.cs
+++ Assets/Script/ScreenShot.cs
@@ -29,7 +29,7 @@ public class ScreenShot : MonoBehaviour
         Destroy(screenTexture);
         if (!Application.isEditor)
         {
-            new NativeShare().AddFile(destination).SetSubject("Subject goes here").SetText("Hellow Meowdle! \nThank you for sharing :) \n" + "#MEOWDLE https://play.google.com/store/apps/details?id=com.blackout.plojectA").Share();
+            //new NativeShare().AddFile(destination).SetSubject("Subject goes here").SetText("Hellow Meowdle! \nThank you for sharing :) \n" + "#MEOWDLE https://play.google.com/store/apps/details?id=com.blackout.plojectA").Share();
             //AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
             //AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
             //intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
